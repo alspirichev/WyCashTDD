527c5ded11299c8011f724bb6b04fa6d35055bad
Alexander Spirichev <>
2019-09-21 18:12:41 +0300

    Add test with multiplication

---
+++ WyCashTDD/Dollar.swift
@@ -0,0 +1,21 @@
+//
+//  Dollar.swift
+//  WyCashTDD
+//
+//  Created by Alexander Spirichev on 21/09/2019.
+//  Copyright © 2019 Alexander Spirichev. All rights reserved.
+//
+
+import Foundation
+
+class Dollar {
+    var amount = 0
+    
+    init(_ amount: Int) {
+        self.amount = amount
+    }
+    
+    func times(_ multiplier: Int) {
+        self.amount *= multiplier
+    }
+}
